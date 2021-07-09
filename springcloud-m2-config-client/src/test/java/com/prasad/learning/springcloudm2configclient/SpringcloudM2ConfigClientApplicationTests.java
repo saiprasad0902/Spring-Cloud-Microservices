08b816af747eb853f8629bffc1470959a62d78ce
Sai Prasad C <saiprasad0902@gmail.com>
2021-07-09 09:20:23 +0530

    Next Commit

---
+++ springcloud-m2-config-client/src/test/java/com/prasad/learning/springcloudm2configclient/SpringcloudM2ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasad.learning.springcloudm2configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudM2ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- springcloud-m2-configserver-git/src/main/java/com/prasad/learning/springcloudm2configservergit/SpringcloudM2ConfigserverGitApplication.java
+++ springcloud-m2-configserver-git/src/main/java/com/prasad/learning/springcloudm2configservergit/SpringcloudM2ConfigserverGitApplication.java
@@ -2,8 +2,10 @@ package com.prasad.learning.springcloudm2configservergit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringcloudM2ConfigserverGitApplication {
 
 	public static void main(String[] args) {
